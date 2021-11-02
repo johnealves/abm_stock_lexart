658d2599fd4f06e38ef95293657eabd05bce4765
johnealves <johne_af@hotmail.com>
2021-11-02 17:11:49 -0300

    feat basic config

---
+++ src/Page/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, MenuItem, Select, TextField } from "@material-ui/core";
+import { Box } from "@material-ui/system";
+
+const Home = () => {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home;
